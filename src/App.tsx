76d6a894bd350385630ec06e99571326e598f5e8
mufaroxyz <81554673+mufaroxyz@users.noreply.github.com>
2024-10-05 11:48:10 +0200

    feat: Improve code structure & fix CSS

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,31 @@
-import React from 'react';
-import './styles/global.css';
-import Home from './components/home';
-import About from './components/about';
-import Projects from './components/projects';
-import Contact from './components/contact';
+import React from "react";
+import Home from "./components/home";
+import About from "./components/about";
+import Projects from "./components/projects";
+import Contact from "./components/contact";
 import MySkills from "./components/myskills.tsx";
 
 const App: React.FC = () => {
-    return (
-        <div>
-            <br/>
-            <h1>Gorgol</h1>
-            <nav>
-                <a href="#home"> Home </a>
-                <a href="#about"> About </a>
-                <a href="#MySkills"> Skills </a>
-                <a href="#projects"> Projects </a>
-                <a href="#contact"> Contact </a>
-            </nav>
+  return (
+    <>
+      <h1 className="header">Gorgol</h1>
+      <nav>
+        <a href="#home"> Home </a>
+        <a href="#about"> About </a>
+        <a href="#MySkills"> Skills </a>
+        <a href="#projects"> Projects </a>
+        <a href="#contact"> Contact </a>
+      </nav>
 
-            <Home/><br/>
-            <About/><br/>
-            <MySkills/><br/>
-            <Projects/><br/>
-            <Contact/><br/>
-        </div>
-    );
+      <div className="page-sections">
+        <Home />
+        <About />
+        <MySkills />
+        <Projects />
+        <Contact />
+      </div>
+    </>
+  );
 };
 
 export default App;
--- src/components/about.tsx
+++ src/components/about.tsx
@@ -1,13 +1,19 @@
-﻿import React from 'react';
-import '../styles/global.css';
+﻿import React from "react";
 
 const About: React.FC = () => {
-    return (
-        <section id="about" className="Section">
-            <h2>About Me</h2>
-            <p>I'm a 17-year-old student from Poland<br/> who loves computer games and night walks<br/>As you can guess, my favourite color is purple preferably dark 😁<br/> I'm not the best at everything I do but I always give it my all 😅</p>
-        </section>
-    );
+  return (
+    <section id="about" className="Section">
+      <h2>About Me</h2>
+      <p>
+        I'm a 17-year-old student from Poland
+        <br /> who loves computer games and night walks
+        <br />
+        As you can guess, my favourite color is purple preferably dark 😁
+        <br /> I'm not the best at everything I do but I always give it my all
+        😅
+      </p>
+    </section>
+  );
 };
 
 export default About;
